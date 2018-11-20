6e3fa361e0153e8bc7dc4b400599d9d86d779213
Nguyen Phan <mangolionstudio@gmail.com>
2018-11-20 12:39:43 -0600

    Removed all html navigation list items
    Placed them in a single JS file navi.js

---
+++ navi.js
@@ -0,0 +1,29 @@
+document.getElementById('listNavi').innerHTML = `
+    <li class="nav-item active">
+        <a class="nav-link" href="#"> HOME </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="courses.html"> COURSES </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#"> EDUCATION </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="projects.html"> PROJECTS </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="experience.html"> RESEARCH EXPERIENCE </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="teachingexperience.html"> TEACHING EXPERIENCE </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#"> PUBLICATIONS </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#"> CONTACT </a>
+    </li>
+`
